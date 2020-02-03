0547c643c4e55a123059589294ad84e1079c10c0
kotesh921 <koti.2839@gmail.com>
2020-02-04 00:14:08 +0530

    2 files modified

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyticsLandingComponent} from './components/analytics-landing/analytics-landing.component';
 import { AnalyticsRoutingModule} from './analytics.routes' ;
+import {SideOptionsModule} from './side-options/side-options.module';
+
 @NgModule({
   imports: [
-    CommonModule, AnalyticsRoutingModule
+    CommonModule, AnalyticsRoutingModule, SideOptionsModule
   ],
   declarations: [AnalyticsLandingComponent]
 })
