5f17f7435096555005e3f81c679defdd9e25b64b
kotesh921 <koti.2839@gmail.com>
2020-02-04 00:05:39 +0530

    2 files added

---
+++ src/app/analytics/analytics.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AnalyticsLandingComponent} from './components/analytics-landing/analytics-landing';
+import { AnalyticsRoutingModule} from './analytics.routes' ;
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AnalyticsLandingComponent]
+})
+export class AnalyticsModule { }
